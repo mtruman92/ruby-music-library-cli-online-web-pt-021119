a97d6b0d8109f74b4b074737fcc71421a7c46c21
Mickae Truman <mtruman@nyit.edu>
2019-04-03 13:15:57 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,21 @@
+#require_relative "../config/environment.rb"
 require 'bundler'
 Bundler.require
 
 module Concerns
+  
+  module Findable
+  
+  def find_by_name(song_name)
+    self.all.detect {|song|songs.name == song_name}
+  end
+  
+  def find_or_create_by_name(song_name)
+    find_by_name(song_name) || create(song_name)
+  end
+
 end
+end
+
 
 require_all 'lib'
---
+++ lib/Genre.rb
@@ -0,0 +1,44 @@
+class Genre 
+  
+  extend Concerns::Findable
+  
+  attr_accessor :name, :songs
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+    save
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.destroy_all 
+    @@all.clear
+  end
+  
+  def save 
+    @@all << self
+    self
+  end
+  
+  def self.create(name)
+    new_genre = Genre.new(name)
+  end
+  
+  def add_song(song)
+    self.songs << song unless @songs.include?(song)
+    self.artist << song.artist unless @songs.include?(song)
+    song.genre = self if song.genre.nil?
+  end
+  
+  def artists 
+    artists = self.songs.collect do |song|song.artist
+  end
+  artists.uniq
+  end
+    
+end
